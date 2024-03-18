4748cd53a468b5de21057bee103dd50a4efe9803
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-03-18 22:02:28 +0530

    Worked with Argument parsing

--- notes-app/app.cjs
+++ notes-app/app.cjs
@@ -3,10 +3,48 @@ const chalk = require('chalk')
 
 const getNotes = require('./notes');
 
-// Now you can use chalk to apply colors and styles to your console output
-console.log(chalk.green.bold('SUCCESS'));
+// create an Add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder:{
+    title:{
+      demandOption: true, // title option is strictly required
+      type: 'string' // expecting a string always for title
+    },
+    body:{
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv){
+    console.log(`Title: ${argv.title} \nBody: ${argv.body}`)
+  }
+})
 
-console.log(process.argv)
-// const command = process.argv[2]
+// create an Remove command
+yargs.command({
+  command: 'remove',
+  handler: function(){
+    console.log('Removing a note')
+  }
+})
 
-console.log(yargs.argv)
+// create an Read command
+yargs.command({
+  command: 'read',
+  handler: function(){
+    console.log('Reading a note')
+  }
+})
+
+// create an add command
+yargs.command({
+  command: 'list',
+  handler: function(){
+    console.log('Listing all the notes')
+  }
+})
+
+yargs.parse() // instead of console.log(yargs.argv)
+
