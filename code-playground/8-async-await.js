0d16f73e74e9fa30349139167bf71745b0024388
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-06-25 21:57:02 +0530

    Chat Application Started

--- code-playground/8-async-await.js
+++ code-playground/8-async-await.js
@@ -73,12 +73,10 @@ addResult()
 const add1 = (a, b) => {
   return new Promise((res, rej) => {
     setTimeout(() => {
-
       if(a < 0 || b < 0) {
         return rej("Numbers must be positive")
       }
       res(a + b)
-
     }, 2000)
   })
 }
