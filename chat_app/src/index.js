c34d75cc38fabb920eb801f9c91077b862cb3c14
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-06-29 22:07:04 +0530

    Chat app started, Socket.IO setup completed and events in socket.io completed

--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -1,7 +1,13 @@
 const path = require('path')
 const express = require("express")
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+
+const server = http.createServer(app)
+
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
@@ -9,10 +15,18 @@ const publicDirectoryPath = path.join(__dirname, "../public")
 
 app.use(express.static(publicDirectoryPath))
 
-// app.get(('/'), (req, res) => {
-//   res.send('Hello')
-// })
+let data = "Welcome!"
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log("New Websocket Connection!")
+
+  socket.emit('message', data)
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message)
+  })
+})
+
+server.listen(port, () => {
   console.log(`Server up and running on http://localhost:${port}`)
 })
